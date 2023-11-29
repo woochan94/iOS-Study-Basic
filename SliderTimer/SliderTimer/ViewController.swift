174774413f97b40a69a795920a7ad857394a72b3
woochan94 <vdncks25@naver.com>
2023-11-29 21:48:50 +0900

    Create SliderTimer project

---
+++ SliderTimer/SliderTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SliderTimer
+//
+//  Created by 정우찬 on 2023/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
