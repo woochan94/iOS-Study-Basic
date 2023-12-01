9f1621be5fa043b644e7f2470d6d7c45545802ad
woochan94 <vdncks25@naver.com>
2023-12-02 00:12:01 +0900

    Add keyboard-related tasks

--- TextFieldProject/TextFieldProject/ViewController.swift
+++ TextFieldProject/TextFieldProject/ViewController.swift
@@ -27,6 +27,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.placeholder = "이메일 입력"
         textField.borderStyle = .roundedRect
         textField.clearButtonMode = .whileEditing
+        // 화면 진입시 키보드 올리기
+        textField.becomeFirstResponder()
         button.layer.cornerRadius = 5
     }
     
@@ -37,8 +39,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return currentLength <= 10
     }
     
+    // 빈화면 클릭시 키보드 내리기
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     @IBAction func doneButtonPressed(_ sender: UIButton) {
-        
+        // 키보드 내리기
+        textField.resignFirstResponder()
     }
     
 }
