3a492c1d7251a1efff462cd8eb2001e500bac276
woochan94 <vdncks25@naver.com>
2023-12-02 01:09:46 +0900

    Configurate View layout programmatically (backgroundColor, autoLayout)

--- LoginProject/LoginProject/ViewController.swift
+++ LoginProject/LoginProject/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let emailTextFieldView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        makeUI()
+    }
+    
+    func makeUI() {
+        view.addSubview(emailTextFieldView)
+        
+        emailTextFieldView.backgroundColor = UIColor.darkGray
+        
+        // autoLayout 설정
+        emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false
+        emailTextFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30.0).isActive = true
+        emailTextFieldView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30.0).isActive = true
+        emailTextFieldView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        emailTextFieldView.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
 
 
