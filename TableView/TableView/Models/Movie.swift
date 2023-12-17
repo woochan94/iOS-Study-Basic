85e735324c611b83c78195f758fbe5fd2e66016d
woochan94 <vdncks25@naver.com>
2023-12-17 20:52:04 +0900

    Make movie tableview

---
+++ TableView/TableView/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  TableView
+//
+//  Created by 정우찬 on 2023/12/17.
+//
+
+import UIKit
+
+struct Movie {
+    var movieImage: UIImage?
+    let movieName: String
+    let movieDescription: String
+}
