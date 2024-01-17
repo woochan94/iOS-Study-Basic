473a171d88552cdedcaa1040ad57f50659e3bcb4
woochan94 <vdncks25@naver.com>
2024-01-18 00:11:17 +0900

    Create music app project

---
+++ MusicApp/MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by 정우찬 on 2024/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
