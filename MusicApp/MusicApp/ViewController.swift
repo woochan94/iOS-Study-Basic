378328c5f5c69fcfa80a88f7a02c40c3833febc9
woochan94 <vdncks25@naver.com>
2024-01-18 00:33:21 +0900

    Apply navigation controller

--- MusicApp/MusicApp/ViewController.swift
+++ MusicApp/MusicApp/ViewController.swift
@@ -11,9 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Title"
     }
-
-
+    
 }
 
